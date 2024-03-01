c58be9eb2bbb993db34c55a60e29488ac554ac6c
Kunal-vani-tech03 <kunaleokhle@vanitechnologies.in>
2024-03-01 16:51:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import OrderCategory from './Pages/FoodCategory/OrderCategory'
+import OderMenu from './Pages/FoodOrder/OderMenu'
+import { Route, Routes, BrowserRouter } from "react-router-dom";
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <BrowserRouter>
+    <Routes>
+    <Route path='/' element={<OrderCategory/>}></Route>
+    <Route path='/order_menu/:category' element={<OderMenu/>}></Route>
+    </Routes>
+    <div>
+    
+    
     </div>
+    </BrowserRouter>
   );
 }
 
